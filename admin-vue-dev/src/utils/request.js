3265725c26d081fc2068675bb7127df6ee53e4bb
RenZhanFeng <947625259@qq.com>
2020-04-14 16:33:15 +0800

    'JWT认证'

--- admin-vue-dev/src/utils/request.js
+++ admin-vue-dev/src/utils/request.js
@@ -12,18 +12,13 @@ const service = axios.create({
 // request interceptor
 service.interceptors.request.use(
   config => {
-    // do something before request is sent
     if (store.getters.token) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
       config.headers['X-Token'] = getToken()
     }
     return config
   },
   error => {
-    // do something with request error
-    console.log(error) // for debug
+    console.log(error)
     return Promise.reject(error)
   }
 )
@@ -33,15 +28,13 @@ service.interceptors.response.use(
   response => {
     const res = response.data
     if (res.code !== 0) {
+      const errMsg = res.msg || '请求失败'
       Message({
-        message: res.message || 'Error',
+        message: errMsg,
         type: 'error',
         duration: 5 * 1000
       })
-
-      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
       if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-        // to re-login
         MessageBox.confirm('Token 已失效，是否重新登录', '确认登出', {
           confirmButtonText: '重新登录',
           cancelButtonText: '取消',
@@ -52,13 +45,14 @@ service.interceptors.response.use(
           })
         })
       }
-      return Promise.reject(new Error(res.message || 'Error'))
+      return Promise.reject(new Error(errMsg))
     } else {
       return res
     }
   },
+
   error => {
-    console.log('err' + error) // for debug
+    console.log('err' + error)
     Message({
       message: error.message,
       type: 'error',
