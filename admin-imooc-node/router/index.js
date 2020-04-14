42c4372ab5632f2f4e6f624f49456090f71233df
RenZhanFeng <947625259@qq.com>
2020-04-14 14:11:15 +0800

    '使用express-validator验证'

--- admin-imooc-node/router/index.js
+++ admin-imooc-node/router/index.js
@@ -29,6 +29,7 @@ router.get('/', function(req, res) {
    * 第二，方法的必须放在路由最后
    */
   router.use((err, req, res, next) => {
+    console.log(err)
     const msg = (err && err.message) || '系统错误'
     const statusCode = (err.output && err.output.statusCode) || 500;
     const errorMsg = (err.output && err.output.payload && err.output.payload.error) || err.message
--- admin-imooc-node/router/user.js
+++ admin-imooc-node/router/user.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const Result = require('../model/Result')
 const { login } = require('../services/user')
+const { md5 } = require('../utils/index')
+const { PWD_SALT } = require('../utils/constant')
+const { body, validationResult } = require('express-validator')
+const boom = require('boom')
+const jwt = require('jsonwebtoken')
+const { PRIVATE_KEY, JWT_EXPIRED } = require('../utils/constant')
 
 const router = express.Router()
 
@@ -9,17 +15,34 @@ router.get('/info', function (req, res, next) {
   res.json('user info...')
 })
 
-router.post('/login', function (req, res, next) {
-  const username = req.body.username
-  const password = req.body.password
-
-  login(username, password).then(user => {
-    if (!user || user.length === 0) {
-      new Result('登录失败').fail(res)
+router.post(
+  '/login',
+  [
+    body('username').isString().withMessage('username类型不正确'),
+    body('password').isString().withMessage('password类型不正确')
+  ],
+  function (req, res, next) {
+    const err = validationResult(req)
+    if (!err.isEmpty()) {
+      const [{ msg }] = err.errors
+      next(boom.badRequest(msg))
     } else {
-      new Result('登录成功').success(res)
+      let { username, password } = req.body
+      password = md5(`${req.body.password}${PWD_SALT}`)
+
+      login(username, password).then(user => {
+        if (!user || user.length === 0) {
+          new Result('登录失败').fail(res)
+        } else {
+          const token = jwt.sign(
+            { username },
+            PRIVATE_KEY,
+            { expiresIn: JWT_EXPIRED }
+          )
+          new Result({ token }, '登录成功').success(res)
+        }
+      })
     }
   })
-})
 
 module.exports = router
